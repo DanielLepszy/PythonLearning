08ee745cbeb1da684b496adf4959b14b379ce117
Daniel Lepszy <Daniel_Lepszy@epam.com>
2021-01-07 14:06:57 +0100

    add logout test case

--- UI_tests_aut/main/page_models/page_models_factory/pages/inventory_page/sections/header_section.py
+++ UI_tests_aut/main/page_models/page_models_factory/pages/inventory_page/sections/header_section.py
@@ -1,6 +1,8 @@
 from seleniumpagefactory import PageFactory
 from selenium import webdriver
 
+from wait_factory.explicit_wait_factory import WaitFactory
+
 
 class HeaderSection(PageFactory):
 
@@ -8,8 +10,16 @@ class HeaderSection(PageFactory):
         super().__init__()
         self.driver = driver
 
-
     locators = {
-        "username_input": ('ID', 'user-name'),
+        "shopping_trolley_icon": ('CSS', '#shopping_cart_container .fa-fw'),
+        "menu_burger_button": ('CSS', '#menu_button_container .bm-burger-button'),
+        "all_items_sidebar": ('ID', 'inventory_sidebar_link'),
+        "about_sidebar": ('ID', 'about_sidebar_link'),
+        "logout_sidebar": ('ID', 'logout_sidebar_link'),
 
     }
+
+    def logout_from_app(self):
+        self.menu_burger_button.click()
+        WaitFactory.wait_until_visibility_of_element(self.driver, self.logout_sidebar)
+        self.logout_sidebar.click()
--- UI_tests_aut/test/pages/inventory_page_tests/test_visibility_of_elements.py
+++ UI_tests_aut/test/pages/inventory_page_tests/test_visibility_of_elements.py
@@ -2,13 +2,17 @@ from test.base.test_case_base import TestCaseBase
 from wait_factory.explicit_wait_factory import WaitFactory
 
 
-class TestVisibilityOfInventoryElements(TestCaseBase):
+class TestLogout(TestCaseBase):
 
     def setup_method(self):
         self.driver = TestCaseBase.get_driver()
-        self.page = TestCaseBase.get_pages_model().get_inventory_page().get_inventory_section()
+        self.page = TestCaseBase.get_pages_model().get_inventory_page()
+        self.page_header = self.page.get_header_section()
 
     def test_visibility_of_elements(self):
-        WaitFactory.wait_until_visibility_of_element(self.driver, self.page.label)
+        if self.page.get_page_url() != self.driver.current_url:
+            raise Exception
+        else:
+            self.page_header.logout_from_app()
 
-        assert self.driver.current_url == self.page.get_page_url()
+        assert self.driver.current_url == 'https://www.saucedemo.com/index.html'
